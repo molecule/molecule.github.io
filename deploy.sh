f17916965e78e6258e6c45e9e4a6de6dd2bd70c0
molecule <molecule@berkeley.edu>
2018-04-11 23:11:11 -0700

    move deploy enc key to parent

--- deploy.sh
+++ deploy.sh
@@ -55,7 +55,7 @@ ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 #openssl aes-256-cbc -K $encrypted_e37f7d53ff65_key -iv $encrypted_e37f7d53ff65_iv -in deploy_key.enc -out deploy_key -d
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out deploy_key -d
 chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
